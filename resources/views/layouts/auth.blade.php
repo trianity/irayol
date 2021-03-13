6b50882594a99a18b2f31a6a5056c128253673aa
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-03-12 21:57:44 -0600

    Cambios en los assets

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,29 +9,15 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>@stack('title')</title>
-
-        <!-- Styles -->
-        <link href="{{ asset('manager/css/custom-style.css') }}" rel="stylesheet" />
-        <link href="{{ asset('manager/AdminLTE-3.0.5/dist/css/adminlte.css') }}" rel="stylesheet" />
-        <link href="{{ asset('manager/AdminLTE-3.0.5/dist/css/skin-midnight.css') }}" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"/>
-        
-        <!-- Google Font: Source Sans Pro -->
-        <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,400i,700" rel="stylesheet">
-        @stack('css')
+        <link rel="stylesheet" href="{{asset('manager/vendor/css/main.css')}}">
     </head>
-    <body class="hold-transition login-page">        
+    <body class="hold-transition login-page">    
 
-        @yield('content')
-
-        <!-- Javascript -->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="{{ asset('manager/AdminLTE-3.0.5/dist/js/adminlte.js') }}"></script>
+        <div id="app">
+            @yield('content')
+        </div>
 
         <!-- Javascript -->
-        @stack('js')
-
+        <script type="text/javascript" src="{{asset('manager/vendor/js/main.js')}}"></script>
     </body>
 </html>
